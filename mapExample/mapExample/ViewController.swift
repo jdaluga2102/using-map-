27f47aef1c8c00ccf25a303319eb2af57fa6d2c4
jdaluga2102 <84146517+jdaluga2102@users.noreply.github.com>
2021-05-27 12:17:26 -0500

    Add files via upload

---
+++ mapExample/mapExample/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  mapExample
+//
+//  Created by period2 on 5/25/21.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+   
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        map.centerCoordinate = CLLocationCoordinate2D(latitude: 42.066148, longitude: -87.937294)
+        map.mapType = .hybrid
+        map.showsCompass = true
+        map.showsTraffic = true
+        map.showsBuildings = true
+        map.showsScale = true
+        map.showsUserLocation = true
+        
+        checkLocationServices()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+            let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: 4000, longitudinalMeters: 4000)
+            map.setRegion(region, animated: true)
+        }
+       
+        func checkLocationAuthorization() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                map.showsUserLocation = true
+                followUserLocation()
+                locationManager.startUpdatingLocation()
+                break
+            case .denied:
+                // Show alert
+                break
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                // Show alert
+                break
+            case .authorizedAlways:
+                break
+            }
+        }
+        
+        func checkLocationServices() {
+            if CLLocationManager.locationServicesEnabled() {
+                setupLocationManager()
+                checkLocationAuthorization()
+            } else {
+                // the user didn't turn it on
+            }
+        }
+        
+        func followUserLocation() {
+            if let location = locationManager.location?.coordinate {
+                let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 4000, longitudinalMeters: 4000)
+                map.setRegion(region, animated: true)
+            }
+        }
+        
+        func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            checkLocationAuthorization()
+        }
+        
+        func setupLocationManager() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        }
+
+}
+
